0dd7b53b437c764999ea0d81493055d768a1b0c9
michaelteng98 <ttweng98@hotmail.com>
2018-05-14 17:19:05 +1000

    This is a new commit

---
+++ mergeconflict.cs
@@ -0,0 +1,24 @@
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+
+        public Person(String firstName, String lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public void SetFirstName(String firstName)
+        {
+            this.firstName = firstName;
+        }
+
+        public void SetLastName(String lastName)
+        {
+            this.lastName = lastName;
+        }
+    }
+}
+
+
